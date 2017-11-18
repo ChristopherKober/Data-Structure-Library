ef10eb854f080bd921cd75db8a0313a5ba3cf72e
ChristopherKober <ChristopherKober@users.noreply.github.com>
2017-11-17 20:20:57 -0600

    Initial Commit

---
+++ Data Structure Library/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Data Structure Library
+//
+//  Created by Kent Kober on 11/17/17.
+//  Copyright © 2017 Kent Kober. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  return 0;
+}
